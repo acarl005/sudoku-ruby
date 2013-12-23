70d76fba19c0815dc0f67857c83cfdff7a0c14c4
Strand McCutchen <strand@bettermistak.es>
2013-12-23 11:16:25 -0800

    Whitespace pass

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,25 +1,25 @@
 class Sudoku
   def initialize(board_string)
   end
- 
+
   def solve!
   end
- 
+
   # Returns a string representing the current state of the board
   # Don't spend too much time on this method; flag someone from staff
   # if you are.
   def board
- 
+
   end
 end
- 
+
 # The file has newlines at the end of each line, so we call
 # String#chomp to remove them.
 board_string = File.readlines('sample.unsolved.txt').first.chomp
- 
+
 game = Sudoku.new(board_string)
- 
+
 # Remember: this will just fill out what it can and not "guess"
 game.solve!
- 
+
 puts game.board
