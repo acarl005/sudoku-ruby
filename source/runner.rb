f12a342b06524c65f9d5aac9caa4b3184f780e9d
Andrew Carlson <acarl005@g.ucla.edu>
2015-04-11 16:25:27 -0700

    added guessing feature

--- source/runner.rb
+++ source/runner.rb
@@ -2,9 +2,13 @@ require_relative 'sudoku'
 
 
 # board_strings = File.readlines('set-01_sudoku_puzzles.txt')
+# solves 13/15 (takes a while)
 board_strings = File.readlines('set-02_project_euler_50-easy-puzzles.txt')
+# solves 50/50
 # board_strings = File.readlines('set-03_peter-norvig_95-hard-puzzles.txt')
+# solves 1/95
 # board_strings = File.readlines('set-04_peter-norvig_11-hardest-puzzles.txt')
+# solves 2/11
 
 board_strings.each_with_index do |board_string, i|
   game = Sudoku.new(board_string.chomp)
@@ -13,6 +17,4 @@ board_strings.each_with_index do |board_string, i|
   puts game.to_p
 end
 
-# game = Sudoku.new('1--92----524-1-----------7--5---81-2---------4-27---9--6-----------3-945----71--6')
-# game.solve
 
--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,11 +1,16 @@
 
 class Sudoku
-  attr_reader :board
+  attr_reader :board, :guesses
   def initialize(board_string)
     @board = []
     board_string.each_char.with_index do |char, index|
       @board << Cell.new(index, char.to_i)
     end
+    @guesses = 0
+  end
+
+  def board_string
+    @board.map { |cell| cell.num || '-' }.join
   end
 
   def cells_in_row(num)
@@ -26,17 +31,65 @@ class Sudoku
     end
   end
 
-  def find_unique(cell_array)
-    possibilities = cell_array.map(&:possibilities)
-    flat = possibilities.flatten
-    flat.each do |number|
-      if flat.count(number) == 1
-        unique_number = number
-        unique_cell = cell_array.find do |cell|
-          cell.possibilities.include?(unique_number)
-        end
-        unique_cell.possibilities = [unique_number]
-        solve_cell(unique_cell)
+  def adjacent_row_boxes(num)
+    if num % 3 == 1
+      [
+        @board.select { |cell| cell.box == num - 1 },
+        @board.select { |cell| cell.box == num + 1 }
+      ]
+    elsif num % 3 == 0
+      [
+        @board.select { |cell| cell.box == num + 2 },
+        @board.select { |cell| cell.box == num + 1 }
+      ]
+    else
+      [
+        @board.select { |cell| cell.box == num - 1 },
+        @board.select { |cell| cell.box == num - 2 }
+      ]
+    end
+  end
+
+  def adjacent_column_boxes(num)
+    if (0..2).include?(num)
+      [
+        @board.select { |cell| cell.box == num + 6 },
+        @board.select { |cell| cell.box == num + 3 }
+      ]
+    elsif (3..5).include?(num)
+      [
+        @board.select { |cell| cell.box == num - 3 },
+        @board.select { |cell| cell.box == num + 3 }
+      ]
+    else
+      [
+        @board.select { |cell| cell.box == num - 6 },
+        @board.select { |cell| cell.box == num - 3 }
+      ]
+    end
+  end
+
+  def solved?
+    numbers = @board.map(&:num)
+    !numbers.include?(nil)
+  end
+
+  def solve
+    counter = 0
+    until solved?
+      check_individuals
+      scan_groups
+      analyze_possi
+      counter += 1
+      break if counter == 10
+    end
+    start_guessing if !solved?
+  end
+
+  def check_individuals
+    @board.each do |cell|
+      if !cell.num
+        try_solve_cell(cell)
       end
     end
   end
@@ -46,50 +99,37 @@ class Sudoku
       .map(&:num)
       .compact
       .each do |number|
-        cell.possibilities.delete(number)
+        cell.possi.delete(number)
       end
     cells_in_row(cell.row)
       .map(&:num)
       .compact
       .each do |number|
-        cell.possibilities.delete(number)
+        cell.possi.delete(number)
       end
     cells_in_box(cell.box)
       .map(&:num)
       .compact
       .each do |number|
-        cell.possibilities.delete(number)
+        cell.possi.delete(number)
       end
-    solve_cell(cell) if cell.possibilities.length == 1
+    solve_cell(cell) if cell.possi.length == 1
   end
 
   def solve_cell(cell)
-    cell.num = cell.possibilities.pop
+    cell.num = cell.possi.pop
     update_relatives(cell)
   end
 
   def update_relatives(cell)
     cells_in_row(cell.row).each do |relative|
-      relative.possibilities.delete(cell.num)
+      relative.possi.delete(cell.num)
     end
     cells_in_column(cell.column).each do |relative|
-      relative.possibilities.delete(cell.num)
+      relative.possi.delete(cell.num)
     end
     cells_in_box(cell.box).each do |relative|
-      relative.possibilities.delete(cell.num)
-    end
-  end
-
-  def solved?
-    numbers = @board.map(&:num)
-    !numbers.include?(nil)
-  end
-
-  def check_individuals
-    @board.each do |cell|
-      if !cell.num
-        try_solve_cell(cell)
-      end
+      relative.possi.delete(cell.num)
     end
   end
 
@@ -105,15 +145,83 @@ class Sudoku
     end
   end
 
-  def solve
-    counter = 0
-    until solved?
-      check_individuals
-      scan_groups
-      counter += 1
-      break if counter == 50
+  def find_unique(cell_array)
+    possi = cell_array.map(&:possi)
+    flat = possi.flatten
+    flat.each do |number|
+      if flat.count(number) == 1
+        unique_number = number
+        unique_cell = cell_array.find do |cell|
+          cell.possi.include?(unique_number)
+        end
+        unique_cell.possi = [unique_number]
+        solve_cell(unique_cell)
+      end
     end
   end
+
+  def analyze_possi
+    @board.each do |cell|
+      if !cell.num
+        examine_row_neighboors(cell)
+      end
+    end
+  end
+
+  def examine_row_neighboors(cell)
+    boxes_of_interest = adjacent_row_boxes(cell.box)
+    remove_these = []
+    cell.possi.each do |num|
+      boxes_of_interest.each do |box|
+        cells_of_interest = box.select do |cell|
+          cell.possi.include?(num)
+        end
+        rows = cells_of_interest.map(&:row).uniq
+        if rows.length == 1 && rows[0] == cell.row
+          remove_these << num
+        end
+      end
+    end
+    remove_these.each { |num| cell.possi.delete(num) }
+  end
+
+  def examine_column_neighboors(cell)
+    boxes_of_interest = adjacent_column_boxes(cell.box)
+    remove_these = []
+    cell.possi.each do |num|
+      boxes_of_interest.each do |box|
+        cells_of_interest = box.select do |cell|
+          cell.possi.include?(num)
+        end
+        columns = cells_of_interest.map(&:column).uniq
+        if columns.length == 1 && columns[0] == cell.column
+          remove_these << num
+        end
+      end
+    end
+    remove_these.each { |num| cell.possi.delete(num) }
+  end
+
+  def start_guessing
+    @guesses += 1
+    guesses = @board.find { |cell| !cell.num }
+                    .possi
+                    .map do |num|
+                      guess = Guess.new(board_string)
+                      guess.guess(num)
+                      guess.solve
+                      guess
+                    end
+    find_correct_guess(guesses)
+  end
+
+  def find_correct_guess(guesses)
+    guesses.select! { |guess| guess.valid }
+    return 'no valid guess' if guesses.empty? || @guesses > 25
+    @board = (guesses.find(&:solved?) || guesses[0]).board
+    start_guessing if !solved?
+  end
+  #recursive FTW
 
   def to_p
     string = <<-BOARD
@@ -132,7 +240,7 @@ class Sudoku
       +-------+-------+-------+
     BOARD
     @board.each { |cell|
-      display = cell.num || "#{cell.possibilities}"
+      display = cell.num || "#{cell.possi}"
       string.sub!(/X/, display.to_s)
     }
     string
@@ -155,7 +263,7 @@ class Sudoku
       +-------+-------+-------+
     BOARD
     @board.each { |cell|
-      display = cell.num || "-"
+      display = cell.num || " "
       string.sub!(/X/, display.to_s)
     }
     string
@@ -163,33 +271,72 @@ class Sudoku
 end
 
 
-class Cell
-  attr_reader :row, :box, :column
-  attr_accessor :num, :possibilities
-
-  def initialize(pos, num)
-    pos = pos.to_s(9)
-    pos = pos.rjust(2, "0")
-
-    num == 0 ? @num = nil : @num = num
-
-    @row = pos[0].to_i
-    @column = pos[1].to_i
-    @box = get_box(pos)
-    @possibilities = (1..9).to_a if !@num
-    @possibilities ||= []
+class Guess < Sudoku
+  attr_reader :valid
+  def initialize(board_string)
+    @valid = true
+    @board = []
+    board_string.each_char.with_index do |char, index|
+      @board << Cell.new(index, char.to_i)
+    end
+    solve
   end
 
-  def get_box(pos)
-    pos_vert = pos.to_i / 30
-    pos_hor = pos[1].to_i / 3
-    [ [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8] ][pos_vert][pos_hor]
+  def find_unique(cell_array)
+    possi = cell_array.map(&:possi)
+    flat = possi.flatten
+    begin
+      flat.each do |number|
+        if flat.count(number) == 1
+          unique_number = number
+          unique_cell = cell_array.find do |cell|
+            cell.possi.include?(unique_number)
+          end
+          unique_cell.possi = [unique_number]
+          solve_cell(unique_cell)
+        end
+      end
+    rescue
+      @valid = false
+    end
+  end
+
+  def guess(num)
+    cell = @board.find { |cell| !cell.num }
+    cell.possi = [num]
+    solve_cell(cell)
+  end
+
+  def solve
+    counter = 0
+    until solved?
+      check_individuals
+      scan_groups
+      analyze_possi
+      counter += 1
+      break if counter == 10
+    end
+  end
+end
+
+
+class Cell
+  attr_reader :row, :box, :column
+  attr_accessor :num, :possi
+  def initialize(str_index, num)
+    @num = num == 0 ? nil : num
+    @row = str_index / 9
+    @column = str_index % 9
+    @box = [ [0, 1, 2],
+             [3, 4, 5],
+             [6, 7, 8] ][row / 3][column / 3]
+    @possi = (1..9).to_a if !@num
+    @possi ||= []
   end
 end
 
 # practice_cell = Cell.new(52, 2)
 # game = Sudoku.new("-3-5--8-45-42---1---8--9---79-8-61-3-----54---5------78-----7-2---7-46--61-3--5--")
-# puts game.solve
-# puts game.find_unique(game.cells_in_row(0))
+# game.solve
+# puts game
+# p game.adjacent_column_boxes(2).map { |box| box.map(&:num) }
